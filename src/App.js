10ecef5f411f15332da85718221316140b7d15b5
naglec13 <naglec13@gmail.com>
2020-10-09 14:34:45 +0300

    - #3 - added jsx structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <h1>What's up buddy</h1>
+    <div>
+      <Navbar />
     </div>
   );
 }
