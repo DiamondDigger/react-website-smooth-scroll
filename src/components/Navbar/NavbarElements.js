675738f262cd153d25f5353c2a7a48119ebeba56
naglec13 <naglec13@gmail.com>
2020-10-09 16:43:20 +0300

    - #3 - added styles for NavbarContainer

--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -11,5 +11,13 @@ export const Nav = styled.nav`
     position: sticky;
     top: 0;
     z-index: 10;
-
+`
+export const NavbarContainer = styled.div`
+    display: flex;
+    justify-content: space-between;
+    height: 80px;
+    width: 100%;
+    max-width: 1100px;
+    padding: 0 24px;
+    z-index: 1;
 `
