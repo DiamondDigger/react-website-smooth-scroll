b644dc05334ac7168102a3e2dbb6e7b0b7eaabcd
naglec13 <naglec13@gmail.com>
2020-10-23 16:36:57 +0300

    - #6 - fixed props in Sidebar components

--- src/components/SideBar/index.js
+++ src/components/SideBar/index.js
@@ -13,18 +13,18 @@ import {
 const Sidebar = ({isOpen, toggle}) => {
     return (
         <SidebarContainer isOpen={isOpen}>
-            <Icon isOpen={isOpen} onClick={toggle}>
+            <Icon onClick={toggle}>
                 <CloseIcon />
             </Icon>
             <SidebarWrapper>
                 <SidebarMenu>
-                    <SidebarLink to='about' isOpen={isOpen} onClick={toggle}>About</SidebarLink>
-                    <SidebarLink to='discover' isOpen={isOpen} onClick={toggle}>Discover</SidebarLink>
-                    <SidebarLink to='services' isOpen={isOpen} onClick={toggle}>Services</SidebarLink>
-                    <SidebarLink to='signup' isOpen={isOpen} onClick={toggle}>Sign Up</SidebarLink>
+                    <SidebarLink to='about' onClick={toggle}>About</SidebarLink>
+                    <SidebarLink to='discover' onClick={toggle}>Discover</SidebarLink>
+                    <SidebarLink to='services' onClick={toggle}>Services</SidebarLink>
+                    <SidebarLink to='signup' onClick={toggle}>Sign Up</SidebarLink>
                 </SidebarMenu>
                 <SideBtnWrap>
-                    <SidebarBtn to='/signin' isOpen={isOpen} onClick={toggle}>Sign In</SidebarBtn>
+                    <SidebarBtn to='/signin' onClick={toggle}>Sign In</SidebarBtn>
                 </SideBtnWrap>
             </SidebarWrapper>
         </SidebarContainer>
