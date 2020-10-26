f67f5892ff4dfb51880834ae4ee0fd72e5e3cb1c
naglec13 <naglec13@gmail.com>
2020-10-26 12:43:02 +0300

    - #7 - added styles for HeroContent, HeroH, HeroP

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,21 +1,42 @@
-import React from 'react'
+import React, { useState } from "react";
 import {
-    HeroContainer,
-    HeroBg,
-    VideoBg
-} from './HeroElements'
-import Video from '../../videos'
+  HeroContainer,
+  HeroBg,
+  VideoBg,
+  HeroContent,
+  HeroH1,
+  HeroP,
+  HeroBtnWrapper,
+  ArrowForward,
+  ArrowRight,
+} from "./HeroElements";
+import Video from "../../videos/video.mp4";
 
 const HeroSection = () => {
-    return (
-        <>
-          <HeroContainer>
-            <HeroBg>
-                <VideoBg autoplay loop full src={Video} type='video/mp4'/>
-            </HeroBg>
-            </HeroContainer>  
-        </>
-    )
-}
+  const [hover, setHover] = useState(false);
 
-export default HeroSection
+  const onHover = () => {
+    setHover(!hover);
+  };
+
+  return (
+    <>
+      <HeroContainer>
+        <HeroBg>
+          <VideoBg autoPlay loop muted src={Video} type="video/mp4" />
+        </HeroBg>
+        <HeroContent>
+          <HeroH1>Some text to subscriber your field of an activity</HeroH1>
+          <HeroP>More info to attract users</HeroP>
+          <HeroBtnWrapper>
+            <Button to="signup" onMouseEnter={onHover} onMouseLeave={onHover}>
+              {hover ? <ArrowRight /> : <ArrowForward />}
+            </Button>
+          </HeroBtnWrapper>
+        </HeroContent>
+      </HeroContainer>
+    </>
+  );
+};
+
+export default HeroSection;
