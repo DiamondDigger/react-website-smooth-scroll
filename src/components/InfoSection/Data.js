a6604b53fb2801655621f3f305a7cbdcdadbc26e
naglec13 <naglec13@gmail.com>
2020-10-28 22:02:41 +0300

    - #8 - added three more Data objects

--- src/components/InfoSection/Data.js
+++ src/components/InfoSection/Data.js
@@ -9,8 +9,59 @@ export const homeObjOne = {
     buttonLabel: 'Get started',
     imgStart: false,
     img: require('../../images/svg-1.svg'),
-    alt: 'Car',
+    alt: 'Card',
     dark: true,
     primary: true,
     darkText: false
+}  
+
+export const homeObjTwo = {
+    id: 'discover',
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: true,
+    topline: 'Unlimited Access',
+    headline: 'Login to your account at any time', 
+    description: 'We have you covered no matter where you are located. All you need is an internet connection and a phone or a computer.',
+    buttonLabel: 'Learn More',
+    imgStart: true,
+    img: require('../../images/svg-2.svg'),
+    alt: 'Man',
+    dark: false,
+    primary: false,
+    darkText: true
+}
+
+export const homeObjThree = {
+    id: 'services',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topline: 'Explore what you want',
+    headline: 'Unlimited Transactions with zero fees', 
+    description: 'Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees.',
+    buttonLabel: 'Learn More',
+    imgStart: false,
+    img: require('../../images/svg-3.svg'),
+    alt: 'Diagrams',
+    dark: true,
+    primary: true,
+    darkText: false
+}
+
+export const homeObjFour = {
+    id: 'signup',
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: true,
+    topline: 'Join our Team',
+    headline: 'Creating an account is extremely easy', 
+    description: 'Get everything set up and ready in 10 under minutes. All you need to do is add your information and you are ready to go.',
+    buttonLabel: 'Start Now',
+    imgStart: false,
+    img: require('../../images/svg-4.svg'),
+    alt: 'Mobile',
+    dark: false,
+    primary: false,
+    darkText: true
 }   
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import HeroSection from "../components/HeroSection";
 import InfoSection from "../components/InfoSection";
-import { homeObjOne } from "../components/InfoSection/Data";
+import { homeObjOne, homeObjTwo, homeObjThree, homeObjFour } from "../components/InfoSection/Data";
 import Navbar from "../components/Navbar";
 import Sidebar from "../components/Sidebar";
 
@@ -17,6 +17,9 @@ const Home = () => {
       <Sidebar isOpen={isOpen} toggle={toggle} />
       <HeroSection />
       <InfoSection {...homeObjOne}/>
+      <InfoSection {...homeObjTwo}/>
+      <InfoSection {...homeObjThree}/>
+      <InfoSection {...homeObjFour}/>
     </>
   );
 };
