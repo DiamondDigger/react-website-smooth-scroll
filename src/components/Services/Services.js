5c296199fef25d91ed9a24941633b6d64cc826c4
naglec13 <naglec13@gmail.com>
2020-10-29 11:39:45 +0300

    - #9 - added jsx

---
+++ src/components/Services/Services.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Services = () => {
+    return (
+          <ServicesContainer>
+              <ServicesH1>Our services</ServicesH1>
+              <ServicesWrapper>
+                  <ServicesCard>
+                      <ServicecIcon src={Icon1}/>
+                      <ServicesH2>Reduce expenses</ServicesH2>
+                      <ServicesP>We help reduce your fess and increase your overall revenue.</ServicesP>
+                  </ServicesCard>
+                  <ServicesCard>
+                      <ServicecIcon src={Icon2}/>
+                      <ServicesH2>Virtual Offices</ServicesH2>
+                      <ServicesP>You can access our platform online anywhere in the world.</ServicesP>
+                  </ServicesCard>
+                  <ServicesCard>
+                      <ServicecIcon src={Icon3}/>
+                      <ServicesH2>Premium Benefits</ServicesH2>
+                      <ServicesP>Unlock our special membership card that returns 5% cash back.</ServicesP>
+                  </ServicesCard>
+              </ServicesWrapper>
+            </ServicesContainer>  
+    )
+}
+
+export default Services
