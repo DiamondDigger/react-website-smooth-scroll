344e0ae4fe2861b967558d7211a99997f04e7f5a
naglec13 <naglec13@gmail.com>
2020-10-30 11:20:37 +0300

    - #11 - added jsx

---
+++ src/components/Footer/index.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <FooterContainer>
+            <FooterWrapper>
+                <FooterLinksContainer>
+                    <FooterLinksWrapper>
+                        <FooterLinkItems>
+                            <FooterLinkTitle>About us</FooterLinkTitle>
+                            <FooterLink to='/signin'>How it works</FooterLink>
+                            <FooterLink to='/signin'>Testimonials</FooterLink>
+                            <FooterLink to='/signin'>Careers</FooterLink>
+                            <FooterLink to='/signin'>Investors</FooterLink>
+                            <FooterLink to='/signin'>Terms of Service</FooterLink>
+                        </FooterLinkItems>
+                    </FooterLinksWrapper>
+                </FooterLinksContainer>
+            </FooterWrapper>
+        </FooterContainer>
+    )
+}
+
+export default Footer
