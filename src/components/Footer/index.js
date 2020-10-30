26a071ebcd9793ee20b22a2e63d4265be7b4fd53
naglec13 <naglec13@gmail.com>
2020-10-30 23:03:03 +0300

    - #11 - added styles

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -14,6 +14,30 @@ const Footer = () => {
                             <FooterLink to='/signin'>Investors</FooterLink>
                             <FooterLink to='/signin'>Terms of Service</FooterLink>
                         </FooterLinkItems>
+                        <FooterLinkTitle>About us</FooterLinkTitle>
+                            <FooterLink to='/signin'>How it works</FooterLink>
+                            <FooterLink to='/signin'>Testimonials</FooterLink>
+                            <FooterLink to='/signin'>Careers</FooterLink>
+                            <FooterLink to='/signin'>Investors</FooterLink>
+                            <FooterLink to='/signin'>Terms of Service</FooterLink>
+                        </FooterLinkItems>
+                    </FooterLinksWrapper>
+                    <FooterLinksWrapper>
+                        <FooterLinkItems>
+                            <FooterLinkTitle>About us</FooterLinkTitle>
+                            <FooterLink to='/signin'>How it works</FooterLink>
+                            <FooterLink to='/signin'>Testimonials</FooterLink>
+                            <FooterLink to='/signin'>Careers</FooterLink>
+                            <FooterLink to='/signin'>Investors</FooterLink>
+                            <FooterLink to='/signin'>Terms of Service</FooterLink>
+                        </FooterLinkItems>
+                        <FooterLinkTitle>About us</FooterLinkTitle>
+                            <FooterLink to='/signin'>How it works</FooterLink>
+                            <FooterLink to='/signin'>Testimonials</FooterLink>
+                            <FooterLink to='/signin'>Careers</FooterLink>
+                            <FooterLink to='/signin'>Investors</FooterLink>
+                            <FooterLink to='/signin'>Terms of Service</FooterLink>
+                        </FooterLinkItems>
                     </FooterLinksWrapper>
                 </FooterLinksContainer>
             </FooterWrapper>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Footer from "../components/Footer";
 import HeroSection from "../components/HeroSection";
 import InfoSection from "../components/InfoSection";
 import { homeObjOne, homeObjTwo, homeObjThree, homeObjFour } from "../components/InfoSection/Data";
@@ -22,6 +23,7 @@ const Home = () => {
       {/* <InfoSection {...homeObjThree}/> */}
       <Services/>
       <InfoSection {...homeObjFour}/>
+       <Footer />
     </>
   );
 };
