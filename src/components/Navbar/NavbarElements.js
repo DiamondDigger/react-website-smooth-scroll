aac22b7e6481b3856ad3a388a347e2a050d1f723
naglec13 <naglec13@gmail.com>
2020-11-02 17:30:30 +0300

    - #12 - added change navbar action

--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -3,7 +3,7 @@ import { Link as LinkRouter } from "react-router-dom";
 import { Link as LinkScroll } from "react-scroll";
 
 export const Nav = styled.nav`
-  background: #010606;
+  background: ${({scrollNav}) => (scrollNav ? '#101522' : 'transparent')};
   height: 80px;
   margin-top: -80px;
   display: flex;
