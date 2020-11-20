12783353d940a2abc3f3f643eb01510e92b18453
naglec13 <naglec13@gmail.com>
2020-11-21 01:54:11 +0300

    - #10 - added styles for FormWrap, Icon, FormContent

--- src/components/Signin/SigninElements.js
+++ src/components/Signin/SigninElements.js
@@ -14,6 +14,42 @@ export const Container = styled.div`
         108deg,
         rgba(1,147,86,1) 0%,
         rgba(10,201,122,1) 110%
-    )
-
+    );
 `
+
+export const FormWrap = styled.div`
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+
+    @media screen and (max-width: 400px) {
+        height: 80%;
+    }
+`
+
+export const Icon = styled(Link)`
+    margin-left: 32px;
+    margin-top: 32px;
+    text-decoration: none;
+    color: #fff;
+    font-weight: 700;
+    font-size: 32px;
+
+    @media screen and (max-width: 480px) {
+        margin-left: 16px;
+        margin-top: 8px;
+    }
+`
+
+export const FormContent = styled.div`
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+
+    @media screen and (max-width: 480px) {
+        padding: 10px;
+    }
+`
+
